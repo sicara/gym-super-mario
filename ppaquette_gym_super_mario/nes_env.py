8678dd8c2a8c1599a6d90f49d554edd767c3f54e
Flavian Hautbois <flavianh@theodo.fr>
2018-02-08 12:17:23 +0100

    Disable output for os.system

--- ppaquette_gym_super_mario/nes_env.py
+++ ppaquette_gym_super_mario/nes_env.py
@@ -385,7 +385,7 @@ class NesEnv(gym.Env, utils.EzPickle):
                         try:
                             cmd = "ps -ef | grep 'fceux' | grep '%s' | grep -v grep | awk '{print \"kill -9\",$2}' | sh -v" % self.temp_lua_path
                             logger.warn('kill prcess %s : %s' % (self.subprocess.pid + 1, cmd))
-                            os.system(cmd)
+                            os.system(cmd + '> /dev/null')
                         except Exception as e:
                             logger.warn('Failed to kill prcess %s %s' % (self.subprocess.pid + 1, e))
                             pass
